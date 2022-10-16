7d95e93c97d34fbd62b9cd3a358558c291fe87fb
kevin <kevin-laurent@hotmail.fr>
2022-10-16 14:47:17 +0200

    archi

---
+++ src/main/java/com/example/creasy/CreasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.creasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/creasy/CreasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
